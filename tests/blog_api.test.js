5b9a9d7d102b9eca415aed37c435ed0f2c1cd798
Jason Fleming <jason.fleming.se@gmail.com>
2021-11-05 07:34:01 -0600

    ex4.8-4.12 tests for blog api

---
+++ tests/blog_api.test.js
@@ -0,0 +1,154 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blog');
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+  },
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[2]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[3]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[4]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[5]);
+  await blogObject.save();
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+}, 100000);
+
+test('there are 5 blogs', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('the first blog is about react patterns', async () => {
+  const response = await api.get('/api/blogs');
+
+  const titles = response.body.map((r) => r.title);
+
+  expect(titles).toContain('React patterns');
+});
+
+test('verify uid is labeled id not _id', async () => {
+  const response = await api.get('/api/blogs');
+  const blog = response.body[0];
+
+  expect(blog).toHaveProperty('id');
+});
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Test Title',
+    author: 'Test Author',
+    url: 'www.testurl.ca',
+    likes: 0,
+  };
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+  const titles = response.body.map((r) => r.title);
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  expect(titles).toContain(newBlog.title);
+});
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Test Author',
+    url: 'www.testurl.ca',
+    likes: 0,
+  };
+  await api.post('/api/blogs').send(newBlog).expect(400);
+
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    title: 'Test Title',
+    author: 'Test Author',
+    likes: 0,
+  };
+  await api.post('/api/blogs').send(newBlog).expect(400);
+
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('blog without likes defaults to 0', async () => {
+  const newBlog = {
+    title: 'Test Title',
+    author: 'Test Author',
+    url: 'www.testurl.ca',
+  };
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+  const blog = response.body[6];
+
+  expect(blog.likes).toBe(0);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
